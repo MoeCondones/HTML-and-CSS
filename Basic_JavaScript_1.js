aea6f3b3c69d86eabbeda01aa116580b6cf48bc5
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2022-05-09 12:30:01 +0100

    Add files via upload

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+<html>
+	<script>
+	function My_First_Function() {
+		var string="kiss me, im irish!";
+		var result=string.fontcolor("green");
+		document.getElementById("irish"). innerHTML=
+		result;
+	}
+	</script>
+</html>
